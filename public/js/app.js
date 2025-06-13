052ae23f1fef6f5dca98203ed6e1b9b564d6a5f6
Zalint <doucoure.saliou@gmail.com>
2025-06-13 23:08:19 +0400

    update the host to render

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : '/api';
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://samabled.onrender.com/api';
 let currentLanguage = 'fr';
 let currentTheme = 'light';
 let isAuthenticated = false;
